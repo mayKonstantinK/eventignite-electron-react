30a659920269b0de83f6ffdd65a5d25aca82d6fa
Konstantin <may.konstantin.work@gmail.com>
2018-08-22 19:34:22 +0600

    Small styles fixies

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Map, ImageOverlay } from 'react-leaflet';
 import {mapImage, mapMaxBounds, mapImageBounds, mapCenter} from '../common/constants';
+import '../styles/MapComponent.css';
 
 class MapComponent extends Component {
     render() {
         return (
-            <Map center={mapCenter} maxBounds={mapMaxBounds} zoom={4} minZoom={4} maxZoom={8} style={{height:"100%", width:"100%",zIndex:2, border: "1px solid red"}}>
+            <Map center={mapCenter} maxBounds={mapMaxBounds} zoom={4} minZoom={4} maxZoom={8} className="Map">
                 <ImageOverlay url={mapImage} bounds={mapImageBounds}/>
             </Map>
         )
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,10 +3,9 @@ import '../styles/App.css';
 import MapComponent from '../components/MapComponent';
 
 class App extends Component {
-
   render() {
     return (
-      <div className="App" style={{border: "1px solid black", width: "100%",height:"100%"}}>
+      <div className="App" style={{width: "100%",height:"100%"}}>
         <MapComponent />
       </div>
     );
